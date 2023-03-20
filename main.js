c9c14230074cb3b2f0b8b1419a55073bf4ac7a13
gonnagetbetter <denissss906@gmail.com>
2023-03-21 00:01:40 +0200

    feature: added second implementation of list CircularList with node class

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 'use strict';
 
-class List {
-  #list = [];
+class Node {
+  constructor(data) {
+      this.data = data;
+      this.next = null;
+  }
+}
+
+class CircularList {
+  constructor() {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+  }
 
   isChar(element) {
     const type = typeof element;
@@ -11,80 +22,159 @@ class List {
   }
 
   getLength() {
-    return this.#list.length;
+      return this.size;
   }
 
   append(element) {
-    if ( this.isChar(element) ) this.#list.push(element);
+      if (this.isChar(element)) {
+          const node = new Node(element);
+          if (!this.head) this.head = node;
+          else this.tail.next = node;
+          node.next = this.head;
+          this.tail = node;
+          this.size++;
+      } else {
+          throw new Error ('Error. Wrong input data type, expected type char.');
+      }
   }
 
   insert(element, index) {
-    if ( this.isChar(element) ) {
-      if (index < 0 || index > this.#list.length) {
-        throw new Error('Error. Incorrect index.');
+      if (this.isChar(element)) {
+          if (index < 0 || index > this.size) throw new Error ('Error. Incorrect index.');;
+          const node = new Node(element);
+          if (index === 0) {
+              node.next = this.head;
+              this.head = node;
+              this.tail = node;
+          } else if (index === this.size) {
+              this.tail.next = node;
+              this.tail = node;
+              node.next = this.head;
+          } else {
+              let current = this.head;
+              for (let i = 0; i < index - 1; i++) {
+                  current = current.next;
+              }
+              node.next = current.next;
+              current.next = node;
+          }
+          this.size++;
+      } else {
+          throw new Error ('Error. Wrong input data type, expected type char.');
       }
-      this.#list.splice(index, 0, element);
-    }
   }
 
   delete(index) {
-    if (index < 0 || index >= this.#list.length) {
-      throw new Error('Error. Index out of range.');
-    }
-    return this.#list.splice(index, 1)[0];
+      if (index < 0 || index >= this.size)  throw new Error ('Error. Index out of range.') ;
+      let deletedItem = null;
+      if (this.size === 1) {
+          deletedItem = this.head.data;
+          this.head = null;
+          this.tail = null;
+      } else if (index === 0) {
+          deletedItem = this.head.data;
+          this.head = this.head.next;
+          this.tail.next = this.head;
+      } else {
+          let current = this.head;
+          for (let i = 0; i < index - 1; i++) {
+              current = current.next;
+          }
+          deletedItem = current.next.data;
+          current.next = current.next.next;
+          if (index === this.size - 1) this.tail = current;
+      }
+      this.size--;
+      return deletedItem;
   }
 
   deleteAll(element) {
-    const newList = this.#list.filter((data) => data !== element);
-    this.#list = newList;
+      let current = this.head;
+      let prev = this.tail;
+      for (let i = 0; i < this.size; i++) {
+          if (current.data === element) {
+              if (i === 0) {
+                  this.head = this.head.next;
+                  this.tail.next = this.head;
+                  prev = this.tail;
+              } else {
+                  prev.next = current.next;
+                  if (i === this.size - 1) this.tail = prev;
+              }
+              this.size--;
+              i--;
+          } else {
+              prev = current;
+          }
+          current = current.next;
+        }
   }
 
   getElement(index) {
-    if (index < 0 || index >= this.#list.length) {
-      throw new Error('Error. Index out of range.');
-    }
-    return this.#list[index];
+      if (index < 0 || index >= this.size) throw new Error('Error. Index out of range.');
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+          current = current.next;
+      }
+      return current.data;
   }
 
   clone() {
-    const newList = new List();
-    for (let i = 0; i < this.#list.length; i++) {
-      newList.append(this.#list[i]);
-    }
-    return newList;
+      const newList = new CircularList();
+      let current = this.head;
+      for (let i = 0; i < this.size; i++) {
+          newList.append(current.data);
+          current = current.next;
+      }
+      return newList;
   }
 
   reverse() {
-    this.#list.reverse();
+      let current = this.head;
+      let prev = this.tail;
+      for (let i = 0; i < this.size; i++) {
+          const next = current.next;
+          current.next = prev;
+          prev = current;
+          current = next;
+      }
+      this.head = prev;
+      if (this.size > 0) this.tail = this.head.next;
   }
 
-  findFirst(data) {
-    for (let i = 0; i < this.#list.length; i++) {
-      if (this.#list[i] === data) return i;
-    }
-    return -1;
+  findFirst(element) {
+      let current = this.head;
+      for (let i = 0; i < this.size; i++) {
+          if (current.data === element) return i;
+          current = current.next;
+      }
+      return -1;
   }
 
-  findLast(data) {
-    for (let i = this.#list.length - 1; i >= 0; i--) {
-      if (this.#list[i] === data) return i;
-    }
-    return -1;
+  findLast(element) {
+      let current = this.head;
+      let lastIndex = -1;
+      for (let i = 0; i < this.size; i++) {
+          if (current.data === element) lastIndex = i;
+          current = current.next;
+      }
+      return lastIndex;
   }
 
   clear() {
-    this.#list = [];
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
   }
 
   extend(list) {
-    if (list instanceof List) {
-      for (let i = 0; i < list.getLength(); i++) {
-        this.isChar(list.getElement(i));
-        this.#list.push(list.getElement(i));
+    if(list instanceof CircularList) {
+      let current = list.head;
+      for (let i = 0; i < list.size; i++) {
+          this.append(current.data);
+          current = current.next;
       }
-    return this.#list;
-  } else throw new Error('Error. Your item must be instance of list to extend it.');
+    } else throw new Error('Error. Your item must be instance of list to extend it.');
+  }
 }
-}
-
-module.exports = List;
+module.exports = CircularList;
